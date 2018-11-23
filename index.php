51f9dadad3855f1fde2501f929e1b3b1ebe0c112
lwvb <leendertvanbeelen@gmail.com>
2018-11-23 22:34:29 +0100

    Basic show my ip version

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
-echo json_encode($_REQUEST);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  include('index.html');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  echo json_encode($_SERVER['REMOTE_ADDR']);
+}
 
