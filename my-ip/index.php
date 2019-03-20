1692c4e7c8e07e74cd90db8d48f768d514173eeb
lwvb <leendertvanbeelen@gmail.com>
2019-03-20 21:41:01 +0100

    Show both ipv4 and ipv6

--- my-ip/index.php
+++ my-ip/index.php
@@ -4,7 +4,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' ) {
   include('view.html');
 }
 
+function getIp() {
+  if($_SERVER['HTTP_X_FORWARDED_FOR']) {
+    return explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0];
+  }
+  return $_SERVER['REMOTE_ADDR'];
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  echo json_encode($_SERVER['HTTP_X_FORWARDED_FOR'] ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
+  echo json_encode(getIp());
 }
 
